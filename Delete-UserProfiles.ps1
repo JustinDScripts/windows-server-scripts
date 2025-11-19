509808dd16c82bf2445158a52862ea6a2e8e427a
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2025-11-19 19:28:43 +0530

    Update Delete-UserProfiles.ps1
    Added another line without -erroraction stop

--- Delete-UserProfiles.ps1
+++ Delete-UserProfiles.ps1
@@ -3,3 +3,5 @@ PowerShell one-liner to delete user profiles with specific strings:
 Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*UserNameEndswith"} | Remove-CimInstance -Confirm:$false
 #>
 try {Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*-A"} | Remove-CimInstance -Confirm:$false -ErrorAction Stop } catch {$_.exception.message}
+
+Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*-A"} | Remove-CimInstance -Confirm:$false -ErrorAction SilentlyContinue
