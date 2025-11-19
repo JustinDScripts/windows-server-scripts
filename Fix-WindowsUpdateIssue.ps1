9ed402689a89a6bbaec4a3d8cb75baaae2f8ca7f
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2025-11-19 20:59:31 +0530

    Update Fix-WindowsUpdateIssue.ps1

--- Fix-WindowsUpdateIssue.ps1
+++ Fix-WindowsUpdateIssue.ps1
@@ -16,8 +16,34 @@ Select Yes
 Read the EULA and accepted it to proceed with data collection
 When the data collection is finished a .zip file will be generated on the OS drive in a folder called MS_DATA
 Upload the .zip file to the workspace for analysis: File Transfer - Case <case #>
-#>
 
+Sample outputs:
+
+C:\WINDOWS\system32>Dism /online /cleanup-image /startcomponentcleanup
+Deployment Image Servicing and Management tool
+Version: 10.0.14393.4169
+Image Version: 10.0.14393.7426
+[==========================100.0%==========================]
+The operation completed successfully.
+
+C:\WINDOWS\system32>Dism /online /cleanup-image /restorehealth
+Deployment Image Servicing and Management tool
+Version: 10.0.14393.4169
+Image Version: 10.0.14393.7426
+[==========================100.0%==========================]
+Error: 0x800f0906
+The source files could not be downloaded.
+Use the "source" option to specify the location of the files that are required to restore the feature. For more information on specifying a source location, see http://go.microsoft.com/fwlink/?LinkId=243077.
+The DISM log file can be found at C:\WINDOWS\Logs\DISM\dism.log
+
+C:\WINDOWS\system32>SFC /scannow
+Beginning system scan.  This process will take some time.
+Beginning verification phase of system scan.
+Verification 100% complete.
+Windows Resource Protection did not find any integrity violations.
+
+#>
+# Commands:
 Dism /online /cleanup-image /startcomponentcleanup
 Dism /online /cleanup-image /restorehealth
 SFC /scannow
