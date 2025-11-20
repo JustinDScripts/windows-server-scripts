f7e4ef5b4dc98a25876239e3afe807055ea987c8
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2025-11-20 19:16:32 +0530

    Update Fix-WindowsUpdateIssue.ps1

--- Fix-WindowsUpdateIssue.ps1
+++ Fix-WindowsUpdateIssue.ps1
@@ -47,3 +47,71 @@ Windows Resource Protection did not find any integrity violations.
 Dism /online /cleanup-image /startcomponentcleanup
 Dism /online /cleanup-image /restorehealth
 SFC /scannow
+<#
+After analyzing the collected data, it was observed the following servicing payload corruption.
+[CBS.log]
+2025-11-19 15:59:25, Info                  CBS   
+=================================
+Checking System Update Readiness.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\mssphtb.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\SearchProtocolHost.exe
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\mssrch.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\Search.ProtocolHandler.MAPI2.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\WSearchMigPlugin.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\tquery.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\SearchIndexer.exe
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\mssitlb.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   wow64_windowssearchengine_31bf3856ad364e35_7.0.14393.206_none_b351c84b3e083664\mssprxy.dll
+Repair failed: Missing replacement payload.
+(p) CSI Payload Corrupt   x86_microsoft-windows-advapi32_31bf3856ad364e35_10.0.14393.0_none_b49e21a1e4d6ebe1\advapi32.dll
+Repair failed: Missing replacement payload.
+Summary:
+Operation: Detect and Repair
+Operation result: 0x800f0906
+Last Successful Step: Entire operation completes.
+Total Detected Corruption: 10
+ CBS Manifest Corruption: 0
+ CBS Metadata Corruption: 0
+ CSI Manifest Corruption: 0
+ CSI Metadata Corruption: 0
+ CSI Payload Corruption: 10
+Total Repaired Corruption: 0
+ CBS Manifest Repaired: 0
+ CSI Manifest Repaired: 0
+ CSI Payload Repaired: 0
+ CSI Store Metadata refreshed: True
+Total Operation Time: 471 seconds.
+2025-11-19 15:59:25, Info                  CBS    CheckSur: hrStatus: 0x800f0906 [CBS_E_DOWNLOAD_FAILURE], download Result: 0x80240438 [Unknown Error]
+2025-11-19 15:59:25, Info                  CBS    Count of times corruption detected: 1
+2025-11-19 15:59:25, Info                  CBS    Seconds between initial corruption detections: -1
+2025-11-19 15:59:25, Info                  CBS    Seconds between corruption and repair: -1
+2025-11-19 15:59:25, Info                  CBS    SQM: Package change report datapoints not populated because SQM is not initialized or not running online.
+2025-11-19 15:59:25, Info                  CBS    Failed to run Detect and repair. [HRESULT = 0x800f0906 - CBS_E_DOWNLOAD_FAILURE]
+2025-11-19 15:59:25, Info                  CBS    Reboot mark cleared
+2025-11-19 15:59:25, Info                  CBS    Winlogon: Simplifying Winlogon CreateSession notifications
+2025-11-19 15:59:25, Info                  CBS    Winlogon: Deregistering for CreateSession notifications
+2025-11-19 15:59:25, Info                  CBS    Exec: Processing complete, session(Corruption Repairing): 31218019_4279730216 [HRESULT = 0x800f0906 - CBS_E_DOWNLOAD_FAILURE]
+2025-11-19 15:59:25, Error                 CBS    Session: 31218019_4279730216 failed to perform store corruption detect and repair operation. [HRESULT = 0x800f0906 - CBS_E_DOWNLOAD_FAILURE]
+2025-11-19 15:59:25, Info                  CBS    Session: 31218019_4279730216 finalized. Download error: 0x80240438 [Unknown Error], Reboot required: no [HRESULT = 0x800f0906 - CBS_E_DOWNLOAD_FAILURE]
+2025-11-19 15:59:25, Info                  CBS    Failed to FinalizeEx using worker session [HRESULT = 0x800f0906]
+
+I've sourced the missing components and have upload them to the workspace under the file sources.zip.
+#>
+# Solution:
+<#
+•	Download and extract sources.zip
+•	Open an elevated command prompt and run:
+o	Dism /online /cleanup-image /restorehealth /source:<Path_to_extracted_sources>
+•	Manually install the latest CU: November 11, 2025—KB5068864 (OS Build 14393.8594) - Microsoft Support
+•	Reboot
+•	Report back the results
+#>
+Dism /online /cleanup-image /restorehealth /source:<Path_to_extracted_sources>
