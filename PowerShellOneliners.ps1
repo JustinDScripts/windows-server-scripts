eec618ff88f5aef1b41e84f41d9f9a275b020097
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2025-11-27 18:48:27 +0530

    Update PowerShellOneliners.ps1

--- PowerShellOneliners.ps1
+++ PowerShellOneliners.ps1
@@ -4,3 +4,11 @@ These are the useful PS1 one-liners useful for Windows
 
 # Change the 'My Computer' icon name in the Desktop to the actual computer name ($env:computername):
 set-itemproperty -path 'registry::HKEY_USERS\S-1-5-21-1343024091-764733703-725345543-71394\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -name '(Default)' -value $($env:computername)
+
+# Set last write time:
+$File = "C:\Temp\MyFile.txt"
+$LastModifiedTime = (Get-Item $File).LastWriteTime
+(Get-Item $File).LastWriteTime = $LastModifiedTime
+# or
+(Get-Item $File).LastWriteTime = (Get-Date "11/27/2025 10:30 AM")
+
