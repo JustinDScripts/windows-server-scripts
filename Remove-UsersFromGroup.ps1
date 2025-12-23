7d28eb13ba8a7c45b2866d3ca56b42f235ec60e0
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2025-12-23 15:13:53 +0530

    Fix variable name and improve user removal logic

--- Remove-UsersFromGroup.ps1
+++ Remove-UsersFromGroup.ps1
@@ -1,11 +1,19 @@
 <#
 Remove-UsersFromGroup.ps1
 Purpose: To remove multiple user accounts from an AD group
+Author: Justin D | 2025-12-23
 Instructions:
 1 Update .\UserNames.txt with the user names that needs to be removed
-2 Yodate $UserGroup = 'GroupName' with the AD group name
+2 Yodate $GroupName = 'GroupName' with the AD group name
 #>
+Push-Location $PSScriptRoot # JD To cd scriptdir
 Import-Module ActiveDirectory
+$UserNamesfile = ".\UserNames.txt"
+$GroupName = 'Local Admin'
+if  (test-path $UserNamesfile -PathType leaf) {
 $Usernames = Get-content ".\UserNames.txt"
-$UserGroup = 'GroupName'
-Remove-ADGroupMember $UserGroup -Members $Usernames -Confirm:$False -Force -whatif
+foreach ($Username in $Usernames) {
+try {Remove-ADGroupMember $GroupName -Members $Username -Confirm:$False -whatif} catch {write-warning "$($_.exception.message)"}
+} # foreach
+} # test-path
+
