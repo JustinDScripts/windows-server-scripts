b4a6d6bdd469cb044bda02746d7b215770372bb5
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2026-01-07 12:25:54 +0530

    Enhance user profile deletion script with permissions
    Added commands to take ownership and grant access to folders before deleting user profiles.

--- Users/Delete-UserProfiles.ps1
+++ Users/Delete-UserProfiles.ps1
@@ -5,3 +5,13 @@ Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*UserNameEn
 try {Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*-A"} | Remove-CimInstance -Confirm:$false -ErrorAction Stop } catch {$_.exception.message}
 
 Get-CimInstance -ClassName Win32_UserProfile | ?{$_.LocalPath -like "*-A"} | Remove-CimInstance -Confirm:$false -ErrorAction SilentlyContinue
+
+# To take ownership
+takeown /F "C:\Path\To\Your\Folder" /R /D Y
+
+# To grant access to administrators:
+icacls "C:\Path\To\Your\Folder" /grant Administrators:F /t /c /q
+icacls "C:\Path\To\Your\Folder" /grant "BUILTIN\Administrators:(OI)(CI)F" /T
+
+# Delete folders under C:\Users
+Get-ChildItem "c:\users" -filter "*-A" -Directory | Remove-Item -Force -Confirm:$false -ErrorAction SilentlyContinue -Recurse
