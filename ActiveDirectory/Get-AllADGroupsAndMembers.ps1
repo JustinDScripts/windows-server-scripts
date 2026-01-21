bf6c79756f7cab7e38c9d8ae2c1816622f7aa628
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2026-01-21 18:07:02 +0530

    Update script to export AD groups and members to dynamic path

---
+++ ActiveDirectory/Get-AllADGroupsAndMembers.ps1
@@ -0,0 +1,36 @@
+# Initial Settings:
+Push-Location $PSScriptRoot # JD To cd scriptdir
+$OutputDir = ".\Outputs\Outputs_$((Get-Date).ToString('yyyy-MM-dd'))"
+$null = New-Item -Path $OutputDir -type directory -Force -ErrorAction SilentlyContinue
+$OutFile = Join-Path -Path  $OutputDir -ChildPath "$((Get-Date).ToString('yyyy-MM-dd_HH-mm-ss')).csv"
+# Initialize a list to store the results
+$results = New-Object System.Collections.Generic.List[System.Object]
+
+# Get all Active Directory groups
+$groups = Get-ADGroup -Filter * -Properties Name
+
+# Loop through each group
+foreach ($group in $groups) {
+write-host "Getting members of group: $group"
+    # Get members of the current group (use -Recursive to include nested group members)
+    $members = Get-ADGroupMember -Identity $group.Name -Recursive | Where-Object {$_.objectClass -eq 'user'} # Filter for only user objects
+
+    # Loop through each member and add to the results list
+    foreach ($member in $members) {
+$MemberProps = Get-ADUser -filter $member -properties PasswordExpired, PasswordLastSet, PasswordNeverExpires,lastlogontimestamp,mail | 
+sort-object PasswordLastSet | 
+select-object Name,GivenName, SamAccountName, UserPrincipalName,Mail,Enabled,lastlogontimestamp,PasswordLastSet,PasswordExpired
+        $results.Add([PSCustomObject]@{
+            GroupName        = $group.Name
+            UserName         = $member.Name
+            UserSamAccountName = $member.SamAccountName
+            Enabled = $MemberProps.Enabled
+            PasswordLastSet = $MemberProps.PasswordLastSet
+            lastlogontimestamp = $MemberProps.lastlogontimestamp
+        })
+    }
+}
+
+# Export the results to a CSV file
+$results | Export-Csv -Path $OutFile -NoTypeInformation -Encoding UTF8
+Write-Host "Export complete. Data saved to $OutFile"
