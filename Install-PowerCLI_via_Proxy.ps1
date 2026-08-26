ca1d3906fda119fa02da1079499d32a19807e045
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2026-08-26 17:29:20 +0530

    Add PowerCLI installation script via proxy

---
+++ Install-PowerCLI_via_Proxy.ps1
@@ -0,0 +1,8 @@
+$proxy = New-Object System.Net.WebProxy('http://145.26.202.186:8080', $true)
+$proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials # DefaultCredentials passes your currently logged-in Windows credentials through to the proxy
+#  $proxy.Credentials = Get-Credential If the proxy needs explicit creds
+[System.Net.WebRequest]::DefaultWebProxy = $proxy
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Install-Module VMware.PowerCLI -Scope CurrentUser -Force -AllowClobber  # This may give error 
+Install-Module VMware.PowerCLI -Scope CurrentUser -Force -AllowClobber -SkipPublisherCheck
